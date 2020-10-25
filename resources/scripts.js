a45fbe3c9aeb67c20a1b50b0e8fd2be378f2c7e9
lampski <jonlamp3@gmail.com>
2020-10-24 23:59:45 -0400

    Mobile parallax fix

--- resources/scripts.js
+++ resources/scripts.js
@@ -9,14 +9,14 @@ function scrollBehavior() {
     }
     parallaxSnow();
 }
-
+document.onload(parallaxSnow());
 function parallaxSnow() {
     let elements = ['background','midground'];
     //for(var i = 0; i < elements.length; i++) {
         //document.getElementById(elements[i]).style.top = ((-1 * window.pageYOffset) /((elements.length-i)*2)+20) + "px";
     //}
     document.getElementById("background").style.top = ((1 * window.pageYOffset)/20) + "px";
-    document.getElementById("midground").style.top = ((-1 * window.pageYOffset)/15) + "px";
+    document.getElementById("midground").style.top = ((-1 * window.pageYOffset)/15)+50 + "px";
 }
 function sendResponse() {
     const contactName= document.getElementById("contact-name").value; 
